088f8b51053f18a6075ca183a63444563fbecef5
Hanzo Huang <h1300923175@gmail.com>
2024-04-19 22:21:19 +0800

    stm32cubemx add gpioc13 led

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
 #define infrared_Pin GPIO_PIN_4
 #define infrared_GPIO_Port GPIOA
 #define M1A_Pin GPIO_PIN_6
